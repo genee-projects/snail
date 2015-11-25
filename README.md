3b85f0134c41b812e295af21419ec07325ea3a29
Rui Ma <rui.ma@geneegroup.com>
2015-11-25 21:05:46 +0800

    update

--- README.md
+++ README.md
@@ -4,4 +4,13 @@
 
 Genee 的 CRM
 
-......
+## 部署文档
+
+傻瓜式部署文档:
+
+* `./install-dependence.sh`. 使用 `composer`, `bower` 安装依赖
+* `./run.sh`. 运行 **crm** 容器
+
+## 假装是 TODO
+
+- [ ] 该做啥
--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ docker-compose up -d
 
 sleep 30
 
-docker exec -it backup-monitor mysql -h 127.0.0.1 -e 'create database monitor'
+docker exec -it crm mysql -h 127.0.0.1 -e 'create database monitor'
 
-docker exec -it backup-monitor php artisan migrate --force
+docker exec -it crm php artisan migrate --force
 
-docker exec -it backup-monitor php artisan db:seed --class=clients --force
+docker exec -it crm php artisan db:seed --class=clients --force
