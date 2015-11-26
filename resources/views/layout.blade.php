6e1269b511e48841551d3884200aa9204e765cd3
Rui Ma <rui.ma@geneegroup.com>
2015-11-26 13:12:26 +0800

    更新名称

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,22 +8,15 @@
     <script src="asserts/3rd/jquery/dist/jquery.min.js"></script>
     <script src="asserts/3rd/angular/angular.min.js"></script>
     <script src="asserts/3rd/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>
-
-    <link href="asserts/3rd/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
+    
+    <script src="asserts/3rd/holderjs/holder.js"></script>
     <link rel="stylesheet" href="asserts/3rd/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="asserts/css/clients.css">
+    <link rel="stylesheet" href="asserts/css/crm.css">
 
-    <link href="asserts/css/sb-admin-2.css" rel="stylesheet">
     <link href="asserts/css/timeline.css" rel="stylesheet">
 
-    <script src="asserts/js/clients.js"></script>
+    <script src="asserts/js/crm.js"></script>
 
-    <script src="asserts/3rd/metisMenu/dist/metisMenu.min.js"></script>
-
-    <script src="asserts/3rd/holderjs/holder.js"></script>
-
-    <script src="asserts/js/sb-admin-2.js"></script>
 
 </head>
 <body>
@@ -31,7 +24,7 @@
     <div id="wrapper">
 
         <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0; position: fixed;">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
@@ -43,57 +36,30 @@
             </div>
             <!-- /.navbar-header -->
 
-
-
             <div class="navbar-default sidebar" role="navigation">
 
 
                 <div class="sidebar-nav navbar-collapse">
 
-                    <ul class="nav" id="side-menu">
+                    <ul class="nav">
 
                         <li>
                             <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> 总览</a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-users fa-fw"></i> 客户管理<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="morris.html">全部</a>
-                                </li>
-                                <li>
-                                    <a href="flot.html">CF 客户</a>
-                                </li>
-                                <li>
-                                    <a href="morris.html">Lims 客户</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
+                            <a href="#"><i class="fa fa-users fa-fw"></i> 客户管理</a>
                         </li>
+
+
                         <li>
-                            <a href="#"><i class="fa fa-server fa-fw"></i> 服务器管理<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="panels-wells.html">Panels and Wells</a>
-                                </li>
-                                <li>
-                                    <a href="buttons.html">Buttons</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
+                            <a href="#"><i class="fa fa-server fa-fw"></i> 服务器管理</a>
+
                         </li>
+
                         <li>
-                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="blank.html">Blank Page</a>
-                                </li>
-                                <li>
-                                    <a href="login.html">Login Page</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
+                            <a href="#"><i class="fa fa-cubes"></i> 产品管理</a>
                         </li>
+
                         <li>
                             <a href="#" id="bars"><i class="fa fa-chevron-circle-left fa-fw"></i> 收起菜单</a>
                         </li>
