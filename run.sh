a375b71a2cc7f57899546f540d581b767184142e
Rui Ma <rui.ma@geneegroup.com>
2015-12-17 17:16:20 +0800

    更新站点运行初始化脚本

--- run.sh
+++ run.sh
@@ -10,4 +10,6 @@ docker exec -it crm mysql -h 127.0.0.1 -e 'create database crm'
 
 docker exec -it crm php artisan migrate --force
 
-docker exec -it crm php artisan db:seed --class=Products --force
+docker exec -it crm php artisan db:seed --class=InitProducts --force
+docker exec -it crm php artisan db:seed --class=InitProductsModules --force
+docker exec -it crm php artisan db:seed --class=InitServerServices --force
