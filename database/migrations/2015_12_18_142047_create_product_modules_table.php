2e1399caf8e48ef7bddcfe3d4d7834b1740bc6b2
Rui Ma <rui.ma@geneegroup.com>
2015-12-21 15:34:05 +0800

    修改 Migration 名称

--- database/migrations/2015_12_18_142047_create_product_modules_table.php
+++ database/migrations/2015_12_18_142047_create_product_modules_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductModuleTable extends Migration
+class CreateProductModulesTable extends Migration
 {
     /**
      * Run the migrations.
