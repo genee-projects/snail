5cc7ca0128b8cb0fcf8aa81a3b4598380407cee0
Rui Ma <rui.ma@geneegroup.com>
2015-12-31 16:49:03 +0800

    优化部署

--- install-dependence.sh
+++ install-dependence.sh
@@ -2,14 +2,5 @@
 
 p=`pwd`
 
-# install 3rd stylesheet fiels and  scripts
-#docker run --rm -v "$p":/usr/src/app stenote/bower install
-
-# build ace
-#docker run --rm -v "$p":/usr/src/app node:latest bash -c "cd /usr/src/app/public/asserts/3rd/ace/ && npm install"
-
-# make something
-#docker run --rm -v "$p":/usr/src/app node:latest node /usr/src/app/public/asserts/3rd/ace/Makefile.dryice.js
-
 # install php dependency
-docker run --rm -v "$p":/usr/src/app stenote/composer install --no-dev
+docker run --rm -v "$p":/usr/src/app stenote/composer install
