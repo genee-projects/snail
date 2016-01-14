7520a255f565bd3bad4fff9cac0b64478522f1fb
Rui Ma <rui.ma@geneegroup.com>
2016-01-14 13:42:43 +0800

    增加错误页面

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,15 @@
+<html><head>
+    <title>Steno Note</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<body>
+<table width="100%" height="100%">
+    <tbody>
+    <tr>
+        <td style="text-align: center;">
+            <p style="font-size: 200px;"> 500 (内部错误) </p>
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+</body></html>
