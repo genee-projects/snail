8ca59887a5b8fa6838df033c7be1d71914ae8f76
Rui Ma <rui.ma@geneegroup.com>
2016-01-20 18:33:15 +0800

    修正 project 页面中显示模块和参数的样式

--- resources/views/projects/profile/softwares.blade.php
+++ resources/views/projects/profile/softwares.blade.php
@@ -4,24 +4,53 @@
     <div class="row">
         <div class="col-sm-12">
             <table class="table-striped table-hover table">
-                <tr>
-                    <td>名称</td>
-                </tr>
 
-                @foreach($project->modules as $module)
-                    <tr>
+                {{--*/
+                //排列模块
+                $default_modules = [];
+                $extra_modules = [];
+
+
+                $product_modules = $project->product->modules;
+
+                foreach($project->modules as $module) {
+                    if ($product_modules->contains($module->id)) {
+                        $default_modules[] = $module;
+                    } else {
+                        $extra_modules[] = $module;
+                    }
+                }
+                /*--}}
+
+
+                @if (count($extra_modules))
+                    <tr class="danger">
+                        <td><h5>附加模块</h5></td>
+                    </tr>
+                @endif
+
+                @foreach($extra_modules as $module)
+                    <tr class="active">
                         <td>
-                            {{--*/ $product_modules = $project->product->modules; $p/*--}}
                             {{ $module->name }}
-
-                            @if ($product_modules->contains($module->id))
-                                <span class="label label-success">
-                                    默认模块
-                                </span>
-                            @endif
                         </td>
                     </tr>
                 @endforeach
+
+                @if (count($default_modules))
+                    <tr class="success show-modules">
+                        <td><h5>默认模块(点击查看)</h5></td>
+                    </tr>
+                @endif
+                @foreach($default_modules as $module)
+                    <tr class="hidden default-module">
+                        <td>
+                            {{ $module->name }}
+                        </td>
+                    </tr>
+                @endforeach
+
+
 
                 {{--*/ $can_manage_module = \Session::get('user')->can('项目模块管理')/*--}}
 
@@ -53,8 +82,8 @@
                                                 @endif
 
                                                 <span data-id="{{ $module->id }}" data-dep-modules="{{ join(',', $module->dep_modules_ids()) }}" class="module-btn btn {{ $btn_class }} text-center">
-                                                                                    {{ $module->name }}
-                                                                                </span>
+                                                    {{ $module->name }}
+                                                </span>
 
                                                 @if($selected)
                                                     <input type="hidden" name="modules[]" value="{{ $module->id }}" />
@@ -167,90 +196,141 @@
     <div class="row">
         <div class="col-sm-12">
             <table class="table-striped table-hover table">
-                <tr>
-                    <td>名称</td>
-                    <td>参数值</td>
-                </tr>
 
-                @foreach($project->params as $param)
+
+                {{--*/
+                $normal_params = [];
+                $spec_params = [];
+
+                foreach($project->params as $param) {
+                    if ($param->pivot->manual) {
+                        $spec_params[] = $param;
+                    } else {
+                        $normal_params[] = $param;
+                    }
+                }
+                /*--}}
+
+                @if (count($spec_params))
+                    <tr class="danger">
+                        <td colspan="2"><h5>特殊参数</h5></td>
+                    </tr>
                     <tr>
-                        <td>
-                            {{ $param->name }}
-                        </td>
-                        <td>
-                            <code>{{ $param->pivot->value }}</code>
-                            @if ($can_manage_param)
-                                <span class="pull-right">
+                        <td>名称</td>
+                        <td>参数值</td>
+                    </tr>
+
+                    @foreach($spec_params as $param)
+                        <tr>
+                            <td>{{ $param->name }}</td>
+                            <td>
+                                <code>{{ $param->pivot->value }}</code>
+                                @if ($can_manage_param)
+                                    <span class="pull-right">
                                     <i class="fa fa-fw fa-edit edit-param edit" data-id="{{ $param->id }}" data-name="{{ $param->name }}" data-value="{{ $param->pivot->value }}"></i>
                                 </span>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
 
-                                <div class="modal fade" id="edit-param" tabindex="-1" role="dialog" aria-labelledby="edit-project-modal-label">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                                <h4 class="modal-title" id="edit-server-modal-label">修改参数信息</h4>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form id="edit-project-param-form" class="form-horizontal" method="post" action="{{ route('project.param.edit', ['id'=> $project->id]) }}">
+                @if (count($normal_params))
+                    <tr class="show-params success">
+                        <td colspan="2"><h5>统一参数(点击查看)</h5></td>
+                    </tr>
 
-                                                    <div class="form-group">
-                                                        <div class="col-sm-12">
-                                                            <input type="text" class="form-control" name="name" disabled="disabled">
-                                                        </div>
-                                                    </div>
+                    <tr class="normal-param">
+                        <td>名称</td>
+                        <td>参数值</td>
+                    </tr>
 
-                                                    <div class="form-group">
-                                                        <div class="col-sm-12">
-                                                            <input type="text" class="form-control" name="value">
-                                                        </div>
-                                                    </div>
+                    @foreach($normal_params as $param)
+                        <tr class="active hidden normal-param">
+                            <td>
+                                {{ $param->name }}
+                            </td>
+                            <td>
+                                <code>{{ $param->pivot->value }}</code>
+                                @if ($can_manage_param)
+                                    <span class="pull-right">
+                                    <i class="fa fa-fw fa-edit edit-param edit" data-id="{{ $param->id }}" data-name="{{ $param->name }}" data-value="{{ $param->pivot->value }}"></i>
+                                </span>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
 
-                                                    <div class="form-group">
-                                                        <div class="col-sm-12">
-                                                            <div class="checkbox">
-                                                                <label>
-                                                                    <input type="checkbox" name="reset"> 恢复默认参数 (勾选该参数后, 上述修改无效)
-                                                                </label>
-                                                            </div>
-                                                        </div>
-                                                    </div>
+            </table>
 
-                                                    <input type="hidden" name="param_id" value="" >
+            <div class="modal fade" id="edit-param" tabindex="-1" role="dialog" aria-labelledby="edit-project-modal-label">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="edit-server-modal-label">修改参数信息</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form id="edit-project-param-form" class="form-horizontal" method="post" action="{{ route('project.param.edit', ['id'=> $project->id]) }}">
 
-                                                </form>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-                                                <button type="submit" class="btn btn-primary" form="edit-project-param-form">修改</button>
-                                            </div>
+                                <div class="form-group">
+                                    <div class="col-sm-12">
+                                        <input type="text" class="form-control" name="name" disabled="disabled">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-sm-12">
+                                        <input type="text" class="form-control" name="value">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-sm-12">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="reset"> 恢复默认参数 (勾选该参数后, 上述修改无效)
+                                            </label>
                                         </div>
                                     </div>
                                 </div>
 
-                                <script type="text/javascript">
+                                <input type="hidden" name="param_id" value="" >
 
-                                    require(['jquery'], function($) {
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+                            <button type="submit" class="btn btn-primary" form="edit-project-param-form">修改</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                                        $('.edit-param').bind('click', function() {
-                                            var $modal = $('#edit-param');
-                                            $modal.find(':input[name=name]').val($(this).data('name'));
-                                            $modal.find(':input[name=value]').val($(this).data('value'));
-                                            $modal.find(':input[name=param_id]').val($(this).data('id'));
+            <script type="text/javascript">
+
+                require(['jquery'], function($) {
+
+                    $('.edit-param').bind('click', function() {
+                        var $modal = $('#edit-param');
+                        $modal.find(':input[name=name]').val($(this).data('name'));
+                        $modal.find(':input[name=value]').val($(this).data('value'));
+                        $modal.find(':input[name=param_id]').val($(this).data('id'));
 
 
-                                            $modal.modal();
-                                        });
+                        $modal.modal();
+                    });
 
-                                    });
-                                </script>
-                            @endif
-                        </td>
-                    </tr>
-                @endforeach
+                    $('.show-params').bind('click', function() {
+                        $(this).parents('table').find('tr.normal-param').removeClass('hidden');
+                    });
 
-            </table>
+                    $('.show-modules').bind('click', function() {
+                        $(this).parents('table').find('tr.default-module').removeClass('hidden');
+                    });
+                });
+            </script>
         </div>
-
     </div>
 </div>
