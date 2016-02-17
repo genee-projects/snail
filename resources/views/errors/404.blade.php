8dfe98f93f9061373643e796c379f28e5deea66a
Rui Ma <rui.ma@geneegroup.com>
2016-02-17 09:41:01 +0800

    Log 参数优化 修改 Error 错误页面

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,12 +1,12 @@
 <html><head>
-    <title>Steno Note</title>
+    <title>CRM</title>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 <body>
 <table width="100%" height="100%">
     <tbody>
     <tr>
         <td style="text-align: center;">
-            <p style="font-size: 200px;"> 404 </p>
+            <p style="font-size: 100px;"> 404 </p>
         </td>
     </tr>
     </tbody>
