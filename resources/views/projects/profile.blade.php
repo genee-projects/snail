566b957bf31e7f2e89a0ebd602a2c9852bfad07e
Rui Ma <rui.ma@geneegroup.com>
2016-02-17 13:31:37 +0800

    对 “部署情况” 先暂时进行隐藏

--- resources/views/projects/profile.blade.php
+++ resources/views/projects/profile.blade.php
@@ -226,12 +226,12 @@
                                 </a>
                             </li>
 
-
-                            <li role="presentation">
-                                <a data-type="trello" href="#trello-tab" role="tab">
-                                    <i class="fa fa-wrench"></i> 部署情况
-                                </a>
-                            </li>
+                            {{--目前部署进度先进行隐藏--}}
+                            {{--<li role="presentation">--}}
+                                {{--<a data-type="trello" href="#trello-tab" role="tab">--}}
+                                    {{--<i class="fa fa-wrench"></i> 部署情况--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
 
                             <li role="presentation">
                                 <a data-type="informations" href="#informations-tab" role="tab">
@@ -239,7 +239,7 @@
                                 </a>
                             </li>
 
-                            <li role="presentation" >
+                            <li role="presentation">
                                 <a data-type="comments" href="#comments-tab" role="tab">
                                     <i class="fa fa-comment-o"></i> 备注信息
                                 </a>
@@ -289,6 +289,7 @@
                                     <i class="fa fa-spinner fa-spin fa-2x"></i>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
