ef4c25be8af50804a54794751a0097a5456260c4
Rui Ma <rui.ma@geneegroup.com>
2016-03-15 10:53:11 +0800

    更新 hardware_item 字段, 删除不必要的 equipment_name 和 equipment_id

--- database/migrations/2016_03_08_145805_create_hardware_item.php
+++ database/migrations/2016_03_08_145805_create_hardware_item.php
@@ -15,8 +15,6 @@ class CreateHardwareItem extends Migration
             $table->increments('id');       // 自增 ID
             $table->integer('hardware_id');     //硬件
             $table->integer('project_id');      //项目 ID
-            $table->string('equipment_name')->nullable();   //仪器名称
-            $table->integer('equipment_id')->nullable();    //仪器 CF-ID
             $table->json('extra');              //附加参数
             $table->tinyInteger('status')->default(\App\HardwareItem::STATUS_ON_THE_WAY);   //默认状态, 在途中
             $table->dateTime('time')->nullable(); // 部署时间
